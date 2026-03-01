e06fda707bab9e39768b55852f022cc5e008b549
BlackMATov <blackmat@mail.ru>
2026-03-02 04:02:29 +0700

    first naive map/set benchmarks

--- develop/benchmarks/map_bmarks.lua
+++ develop/benchmarks/map_bmarks.lua
@@ -1,11 +1,37 @@
 local immut = require 'immut'
 local basics = require 'develop.basics'
 
-local N = 1000
+local N = 100
 
 print '----------------------------------------'
 
 basics.describe_bench(
-    string.format('Map Benchmarks: %d', N),
+    string.format('Map Benchmarks: Insert %d random elements', N),
     function()
+        local m = immut.map()
+
+        for _ = 1, N do
+            local k = math.random(1, N)
+            m = m:insert(k, k)
+        end
+    end)
+
+basics.describe_bench(
+    string.format('Map Benchmarks: Insert %d ascending elements', N),
+    function()
+        local m = immut.map()
+
+        for i = 1, N do
+            m = m:insert(i, i)
+        end
+    end)
+
+basics.describe_bench(
+    string.format('Map Benchmarks: Insert %d descending elements', N),
+    function()
+        local m = immut.map()
+
+        for i = N, 1, -1 do
+            m = m:insert(i, i)
+        end
     end)
--- develop/benchmarks/set_bmarks.lua
+++ develop/benchmarks/set_bmarks.lua
@@ -1,11 +1,37 @@
 local immut = require 'immut'
 local basics = require 'develop.basics'
 
-local N = 1000
+local N = 100
 
 print '----------------------------------------'
 
 basics.describe_bench(
-    string.format('Set Benchmarks: %d', N),
+    string.format('Set Benchmarks: Insert %d random elements', N),
     function()
+        local s = immut.set()
+
+        for _ = 1, N do
+            local k = math.random(1, N)
+            s = s:insert(k)
+        end
+    end)
+
+basics.describe_bench(
+    string.format('Set Benchmarks: Insert %d ascending elements', N),
+    function()
+        local s = immut.set()
+
+        for i = 1, N do
+            s = s:insert(i)
+        end
+    end)
+
+basics.describe_bench(
+    string.format('Set Benchmarks: Insert %d descending elements', N),
+    function()
+        local s = immut.set()
+
+        for i = N, 1, -1 do
+            s = s:insert(i)
+        end
     end)
