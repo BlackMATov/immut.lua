f5a58da42ada9fb0b7c58499a4dcfc767f9c8e8e
BlackMATov <blackmat@mail.ru>
2026-03-10 05:09:41 +0700

    add some prepared functions for key hashing and bit counting

--- immut.lua
+++ immut.lua
@@ -27,6 +27,18 @@ local immut = {
     ]]
 }
 
+local __lua_error = error
+local __lua_type = type
+
+local __lua_string_byte = string.byte
+local __lua_string_format = string.format
+
+local __immut_num_hash
+local __immut_str_hash
+
+local __immut_popcount8_lut
+local __immut_popcount32_fun
+
 ---
 ---
 --- LIST API
@@ -40,50 +52,50 @@ __list_mt.__index = __list_mt
 ---Returns the number of elements in the list.
 ---@return integer
 ---@nodiscard
-function __list_mt:size() error 'not implemented' end
+function __list_mt:size() __lua_error 'not implemented' end
 
 ---Returns `true` if the list contains no elements, `false` otherwise.
 ---@return boolean
 ---@nodiscard
-function __list_mt:empty() error 'not implemented' end
+function __list_mt:empty() __lua_error 'not implemented' end
 
 ---Retrieves the first element of the list.
 ---If the list is empty, it returns `nil`.
 ---@return any
 ---@nodiscard
-function __list_mt:head() error 'not implemented' end
+function __list_mt:head() __lua_error 'not implemented' end
 
 ---Retrieves the last element of the list.
 ---If the list is empty, it returns `nil`.
 ---@return any
 ---@nodiscard
-function __list_mt:last() error 'not implemented' end
+function __list_mt:last() __lua_error 'not implemented' end
 
 ---Retrieves a new list containing all elements of the original list except the first one.
 ---If the list is empty, it returns `nil`.
 ---@return immut.list
 ---@nodiscard
-function __list_mt:tail() error 'not implemented' end
+function __list_mt:tail() __lua_error 'not implemented' end
 
 ---Retrieves a new list containing all elements of the original list except the last one.
 ---If the list is empty, it returns `nil`.
 ---@return immut.list
 ---@nodiscard
-function __list_mt:init() error 'not implemented' end
+function __list_mt:init() __lua_error 'not implemented' end
 
 ---Returns a new list with a given element added to the front of the list.
 ---@param head any
 ---@return immut.list
 ---@nodiscard
 ---@diagnostic disable-next-line: unused-local
-function __list_mt:cons(head) error 'not implemented' end
+function __list_mt:cons(head) __lua_error 'not implemented' end
 
 ---Returns a new list with a given element added to the end of the list.
 ---@param last any
 ---@return immut.list
 ---@nodiscard
 ---@diagnostic disable-next-line: unused-local
-function __list_mt:snoc(last) error 'not implemented' end
+function __list_mt:snoc(last) __lua_error 'not implemented' end
 
 ---
 ---
@@ -98,12 +110,12 @@ __dict_mt.__index = __dict_mt
 ---Returns the number of key-value pairs in the dict.
 ---@return integer
 ---@nodiscard
-function __dict_mt:size() error 'not implemented' end
+function __dict_mt:size() __lua_error 'not implemented' end
 
 ---Returns `true` if the dict contains no key-value pairs, `false` otherwise.
 ---@return boolean
 ---@nodiscard
-function __dict_mt:empty() error 'not implemented' end
+function __dict_mt:empty() __lua_error 'not implemented' end
 
 ---Associates a given key with a value in the dict, returning a new dict instance with the updated key-value pair.
 ---If the key already exists, its value is replaced with the new value.
@@ -112,7 +124,7 @@ function __dict_mt:empty() error 'not implemented' end
 ---@return immut.dict
 ---@nodiscard
 ---@diagnostic disable-next-line: unused-local
-function __dict_mt:assoc(key, value) error 'not implemented' end
+function __dict_mt:assoc(key, value) __lua_error 'not implemented' end
 
 ---Dissociates a given key from the dict, returning a new dict instance without the specified key.
 ---If the key does not exist, the original dict is returned unchanged.
@@ -120,7 +132,7 @@ function __dict_mt:assoc(key, value) error 'not implemented' end
 ---@return immut.dict
 ---@nodiscard
 ---@diagnostic disable-next-line: unused-local
-function __dict_mt:dissoc(key) error 'not implemented' end
+function __dict_mt:dissoc(key) __lua_error 'not implemented' end
 
 ---Retrieves the value associated with a given key in the dict.
 ---If the key does not exist, it returns `nil`.
@@ -128,14 +140,14 @@ function __dict_mt:dissoc(key) error 'not implemented' end
 ---@return any
 ---@nodiscard
 ---@diagnostic disable-next-line: unused-local
-function __dict_mt:lookup(key) error 'not implemented' end
+function __dict_mt:lookup(key) __lua_error 'not implemented' end
 
 ---Checks if a given key exists in the dict, returning `true` if it does and `false` otherwise.
 ---@param key any
 ---@return boolean
 ---@nodiscard
 ---@diagnostic disable-next-line: unused-local
-function __dict_mt:contains(key) error 'not implemented' end
+function __dict_mt:contains(key) __lua_error 'not implemented' end
 
 ---
 ---
@@ -155,7 +167,7 @@ local __empty_lists = {}
 ---@return immut.list
 ---@nodiscard
 function immut.list(mode)
-    return __empty_lists[mode] or error(string.format(
+    return __empty_lists[mode] or __lua_error(__lua_string_format(
         'invalid list mode: %s, expected one of: %s',
         tostring(mode), table.concat(immut.AVAILABLE_LIST_MODES, ', ')))
 end
@@ -179,7 +191,7 @@ local __empty_dicts = {}
 ---@return immut.dict
 ---@nodiscard
 function immut.dict(mode)
-    return __empty_dicts[mode] or error(string.format(
+    return __empty_dicts[mode] or __lua_error(__lua_string_format(
         'invalid dict mode: %s, expected one of: %s',
         tostring(mode), table.concat(immut.AVAILABLE_DICT_MODES, ', ')))
 end
@@ -363,9 +375,23 @@ end
 
 ---@class immut.hamt_node
 
+---@class immut.hamt_node_leaf : immut.hamt_node
+
+---@class immut.hamt_node_bitmap : immut.hamt_node
+
+---@class immut.hamt_node_collision : immut.hamt_node
+
 ---@diagnostic disable-next-line: unused-local
 local function __hamt_hash(key)
-    return 0
+    local key_type = __lua_type(key)
+
+    if key_type == 'number' then
+        return __immut_num_hash(key)
+    elseif key_type == 'string' then
+        return __immut_str_hash(key)
+    else
+        __lua_error(__lua_string_format('unsupported key type for hamt dict: %s', key_type))
+    end
 end
 
 ---@param node immut.hamt_node
@@ -460,6 +486,162 @@ end
 
 function __hamt_dict_mt:contains(key)
     return __hamt_lookup(self.__root, 0, key, __hamt_hash(key)) ~= nil
+end
+
+---
+---
+--- NUMBER HASHING
+---
+---
+
+---@param v number
+---@return integer
+---@nodiscard
+function __immut_num_hash(v)
+    if v * 0 ~= 0 then
+        if v ~= v then return 0xDEADBEEF end
+        return v < 0 and 0xCAFEBABE or 0xBABECAFE
+    end
+
+    local h = 0x517CC1B7
+
+    if v < 0 then
+        h = 0x85EBCA77
+        v = -v
+    end
+
+    local f_part = v % 1
+
+    if f_part == 0 then
+        local i_part_lo = v % 2 ^ 32
+        local i_part_hi = (v - i_part_lo) / 2 ^ 32
+
+        h = (h * 0x19660D + i_part_lo * 1013 + i_part_hi) % 2 ^ 32
+
+        local t = (h - h % 2 ^ 16) / 2 ^ 16
+        h = (h + t * 0x9E3779B1) % 2 ^ 32
+
+        h = (h * 0x19660D) % 2 ^ 32
+    else
+        v = v - f_part
+
+        local i_part_lo = v % 2 ^ 32
+        local i_part_hi = (v - i_part_lo) / 2 ^ 32
+
+        f_part = f_part * 2 ^ 32
+        local f_part_hi = f_part - f_part % 1
+        f_part = (f_part - f_part_hi) * 2 ^ 32
+        local f_part_lo = f_part - f_part % 1
+
+        h = (h * 0x19660D + i_part_lo * 1013 + i_part_hi) % 2 ^ 32
+
+        local t = (h - h % 2 ^ 16) / 2 ^ 16
+        h = (h + t * 0x9E3779B1) % 2 ^ 32
+
+        h = (h * 0x19660D + f_part_hi * 1013 + f_part_lo) % 2 ^ 32
+    end
+
+    return h
+end
+
+---
+---
+--- STRING HASHING
+---
+---
+
+---@param s string
+---@return integer
+---@nodiscard
+function __immut_str_hash(s)
+    local i, l, h = 1, #s, 5381
+
+    while i <= l - 15 do
+        local c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11, c12, c13, c14, c15, c16 = __lua_string_byte(s, i, i + 15)
+
+        h = h * 33 ^ 4 + c1 * 33 ^ 3 + c2 * 33 ^ 2 + c3 * 33 + c4
+        h = h % 2 ^ 32
+
+        h = h * 33 ^ 4 + c5 * 33 ^ 3 + c6 * 33 ^ 2 + c7 * 33 + c8
+        h = h % 2 ^ 32
+
+        h = h * 33 ^ 4 + c9 * 33 ^ 3 + c10 * 33 ^ 2 + c11 * 33 + c12
+        h = h % 2 ^ 32
+
+        h = h * 33 ^ 4 + c13 * 33 ^ 3 + c14 * 33 ^ 2 + c15 * 33 + c16
+        h = h % 2 ^ 32
+
+        i = i + 16
+    end
+
+    while i <= l - 7 do
+        local c1, c2, c3, c4, c5, c6, c7, c8 = __lua_string_byte(s, i, i + 7)
+
+        h = h * 33 ^ 4 + c1 * 33 ^ 3 + c2 * 33 ^ 2 + c3 * 33 + c4
+        h = h % 2 ^ 32
+
+        h = h * 33 ^ 4 + c5 * 33 ^ 3 + c6 * 33 ^ 2 + c7 * 33 + c8
+        h = h % 2 ^ 32
+
+        i = i + 8
+    end
+
+    while i <= l - 3 do
+        local c1, c2, c3, c4 = __lua_string_byte(s, i, i + 3)
+
+        h = h * 33 ^ 4 + c1 * 33 ^ 3 + c2 * 33 ^ 2 + c3 * 33 + c4
+        h = h % 2 ^ 32
+
+        i = i + 4
+    end
+
+    while i <= l - 1 do
+        local c1, c2 = __lua_string_byte(s, i, i + 1)
+
+        h = h * 33 ^ 2 + c1 * 33 + c2
+        h = h % 2 ^ 32
+
+        i = i + 2
+    end
+
+    while i <= l do
+        local c = __lua_string_byte(s, i)
+
+        h = h * 33 + c
+        h = h % 2 ^ 32
+
+        i = i + 1
+    end
+
+    return h
+end
+
+---
+---
+--- BIT COUNTING
+---
+---
+
+__immut_popcount8_lut = {
+    0, 1, 1, 2, 1, 2, 2, 3, 1, 2, 2, 3, 2, 3, 3, 4, 1, 2, 2, 3, 2, 3, 3, 4, 2, 3, 3, 4, 3, 4, 4, 5,
+    1, 2, 2, 3, 2, 3, 3, 4, 2, 3, 3, 4, 3, 4, 4, 5, 2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6,
+    1, 2, 2, 3, 2, 3, 3, 4, 2, 3, 3, 4, 3, 4, 4, 5, 2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6,
+    2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6, 3, 4, 4, 5, 4, 5, 5, 6, 4, 5, 5, 6, 5, 6, 6, 7,
+    1, 2, 2, 3, 2, 3, 3, 4, 2, 3, 3, 4, 3, 4, 4, 5, 2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6,
+    2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6, 3, 4, 4, 5, 4, 5, 5, 6, 4, 5, 5, 6, 5, 6, 6, 7,
+    2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6, 3, 4, 4, 5, 4, 5, 5, 6, 4, 5, 5, 6, 5, 6, 6, 7,
+    3, 4, 4, 5, 4, 5, 5, 6, 4, 5, 5, 6, 5, 6, 6, 7, 4, 5, 5, 6, 5, 6, 6, 7, 5, 6, 6, 7, 6, 7, 7, 8,
+}
+
+function __immut_popcount32_fun(v)
+    local pc8 = __immut_popcount8_lut
+
+    local bb1 = v % 2 ^ 8; v = (v - bb1) / 2 ^ 8
+    local bb2 = v % 2 ^ 8; v = (v - bb2) / 2 ^ 8
+    local bb3 = v % 2 ^ 8; v = (v - bb3) / 2 ^ 8
+    local bb4 = v
+
+    return pc8[bb1 + 1] + pc8[bb2 + 1] + pc8[bb3 + 1] + pc8[bb4 + 1]
 end
 
 ---
