66570b19666e92d06de9d691382132f4672ae263
BlackMATov <blackmat@mail.ru>
2026-03-15 21:34:30 +0700

    new fuzzing dict test

--- develop/fuzzing/dict_fuzz.lua
+++ develop/fuzzing/dict_fuzz.lua
@@ -147,4 +147,97 @@ for _, mode in ipairs(immut.AVAILABLE_DICT_MODES) do
             curr_dict = next_dict
         end
     end
+
+    do
+        local curr_dict = immut.dict(mode)
+
+        local expected_key_values = {} ---@type table<any, any>
+        local expected_size = 0 ---@type integer
+
+        for _ = 1, ASSOC_COUNT + DISSOC_COUNT do
+            local r = math.random(1, 2)
+
+            if r == 1 then
+                local new_key = random_key_or_value()
+                local new_value = random_key_or_value()
+
+                local next_dict = curr_dict:assoc(new_key, new_value)
+
+                for _, key in ipairs(POSSIBLE_KEYS_OR_VALUES) do
+                    if expected_key_values[key] ~= nil then
+                        assert(curr_dict:contains(key) == true)
+                        assert(curr_dict:lookup(key) == expected_key_values[key])
+                    else
+                        assert(curr_dict:contains(key) == false)
+                        assert(curr_dict:lookup(key) == nil)
+                    end
+                end
+
+                if expected_key_values[new_key] ~= nil then
+                    if expected_key_values[new_key] == new_value then
+                        assert(curr_dict == next_dict)
+                    else
+                        assert(curr_dict ~= next_dict)
+                        expected_key_values[new_key] = new_value
+                    end
+                else
+                    assert(curr_dict ~= next_dict)
+                    expected_key_values[new_key] = new_value
+                    expected_size = expected_size + 1
+                end
+
+                assert(next_dict:size() == expected_size)
+
+                for _, key in ipairs(POSSIBLE_KEYS_OR_VALUES) do
+                    if expected_key_values[key] ~= nil then
+                        assert(next_dict:contains(key) == true)
+                        assert(next_dict:lookup(key) == expected_key_values[key])
+                    else
+                        assert(next_dict:contains(key) == false)
+                        assert(next_dict:lookup(key) == nil)
+                    end
+                end
+
+                curr_dict = next_dict
+            elseif r == 2 then
+                local rem_key = random_key_or_value()
+
+                local next_dict = curr_dict:dissoc(rem_key)
+
+                for _, key in ipairs(POSSIBLE_KEYS_OR_VALUES) do
+                    if expected_key_values[key] ~= nil then
+                        assert(curr_dict:contains(key) == true)
+                        assert(curr_dict:lookup(key) == expected_key_values[key])
+                    else
+                        assert(curr_dict:contains(key) == false)
+                        assert(curr_dict:lookup(key) == nil)
+                    end
+                end
+
+                if expected_key_values[rem_key] ~= nil then
+                    assert(curr_dict ~= next_dict)
+
+                    expected_key_values[rem_key] = nil
+                    expected_size = expected_size - 1
+                else
+                    assert(curr_dict == next_dict)
+                end
+
+                assert(next_dict:size() == expected_size)
+                assert(next_dict:empty() == (expected_size == 0))
+
+                for _, key in ipairs(POSSIBLE_KEYS_OR_VALUES) do
+                    if expected_key_values[key] ~= nil then
+                        assert(next_dict:contains(key) == true)
+                        assert(next_dict:lookup(key) == expected_key_values[key])
+                    else
+                        assert(next_dict:contains(key) == false)
+                        assert(next_dict:lookup(key) == nil)
+                    end
+                end
+
+                curr_dict = next_dict
+            end
+        end
+    end
 end
