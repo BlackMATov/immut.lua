ef1cedef50a6c8bab6fa4a5f5a9ae199f0143453
BlackMATov <blackmat@mail.ru>
2026-03-17 05:24:01 +0700

    isll additional tests

--- develop/testing/list_tests.lua
+++ develop/testing/list_tests.lua
@@ -132,4 +132,58 @@ for _, mode in ipairs(immut.AVAILABLE_LIST_MODES) do
             assert(l3:tail():tail():last() == 84)
         end
     end
+
+    do
+        local l = immut.list(mode):cons(1):cons(2):cons(3)
+        assert(l:size() == 3 and not l:empty())
+
+        do
+            local l_tail = l:tail()
+            assert(l_tail:size() == 2 and not l_tail:empty())
+            assert(l_tail:head() == 2 and l_tail:last() == 1)
+
+            local l_tail_tail = l_tail:tail()
+            assert(l_tail_tail:size() == 1 and not l_tail_tail:empty())
+            assert(l_tail_tail:head() == 1 and l_tail_tail:last() == 1)
+
+            local l_tail_tail_tail = l_tail_tail:tail()
+            assert(l_tail_tail_tail:size() == 0 and l_tail_tail_tail:empty())
+
+            local l_tail_tail_tail_tail = l_tail_tail_tail:tail()
+            assert(l_tail_tail_tail_tail == nil)
+        end
+
+        do
+            local l_init = l:init()
+            assert(l_init:size() == 2 and not l_init:empty())
+            assert(l_init:head() == 3 and l_init:last() == 2)
+
+            local l_init_init = l_init:init()
+            assert(l_init_init:size() == 1 and not l_init_init:empty())
+            assert(l_init_init:head() == 3 and l_init_init:last() == 3)
+
+            local l_init_init_init = l_init_init:init()
+            assert(l_init_init_init:size() == 0 and l_init_init_init:empty())
+
+            local l_init_init_init_init = l_init_init_init:init()
+            assert(l_init_init_init_init == nil)
+        end
+    end
+
+    do
+        local l = immut.list(mode)
+        assert(l:size() == 0 and l:empty())
+
+        local l1 = l:snoc(1)
+        assert(l1:size() == 1 and not l1:empty())
+        assert(l1:head() == 1 and l1:last() == 1)
+
+        local l2 = l1:snoc(2)
+        assert(l2:size() == 2 and not l2:empty())
+        assert(l2:head() == 1 and l2:last() == 2)
+
+        local l3 = l2:snoc(3)
+        assert(l3:size() == 3 and not l3:empty())
+        assert(l3:head() == 1 and l3:last() == 3)
+    end
 end
